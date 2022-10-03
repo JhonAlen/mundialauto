5f8b11aaae9e6532efc126dd76de6b61444cb2fe
Thomas Lodato <tlodato@compuamerica.com.ve>
2022-10-03 15:37:31 -0400

    [fix]: Fixing Merge Problems in App-routing and App Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -276,16 +276,13 @@ import { FleetLoadingComponent } from './subscription/fleet-loading/fleet-loadin
 import { CollectionIndexComponent } from './administration/collection/collection-index/collection-index.component';
 import { CollectionDetailComponent } from './administration/collection/collection-detail/collection-detail.component';
 import { AdministrationPaymentComponent } from './pop-up/administration-payment/administration-payment.component';
-<<<<<<< HEAD
 import { PlanRcvIndexComponent } from './products/plan-rcv/plan-rcv-index/plan-rcv-index.component';
 import { PlanRcvDetailComponent } from './products/plan-rcv/plan-rcv-detail/plan-rcv-detail.component';
-=======
 import { FleetContractIndividualDetailComponent } from './subscription/fleet-contract-individual/fleet-contract-individual-detail/fleet-contract-individual-detail.component';
 import { FleetContractIndividualIndexComponent } from './subscription/fleet-contract-individual/fleet-contract-individual-index/fleet-contract-individual-index.component';
 
 
 
->>>>>>> aa8f84740817d3c34bfaa2bb643b2c339db7c1d4
 
 export function HttpLoaderFactory(http: HttpClient){
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -552,13 +549,10 @@ export function HttpLoaderFactory(http: HttpClient){
     CollectionIndexComponent,
     CollectionDetailComponent,
     AdministrationPaymentComponent,
-<<<<<<< HEAD
     PlanRcvIndexComponent,
-    PlanRcvDetailComponent
-=======
+    PlanRcvDetailComponent,
     FleetContractIndividualDetailComponent,
     FleetContractIndividualIndexComponent
->>>>>>> aa8f84740817d3c34bfaa2bb643b2c339db7c1d4
   ],
   imports: [
     BrowserModule,
