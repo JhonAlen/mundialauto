a658dcd76ee77382c6414260f0dae27d78d870e0
Jhon Alen <jalen@compuamerica.com.ve>
2023-01-09 15:25:31 -0400

    se implemento pantalla para las anulaciones de las polizas

---
+++ src/app/pop-up/cause-for-cancellation/cause-for-cancellation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cause-for-cancellation',
+  templateUrl: './cause-for-cancellation.component.html',
+  styleUrls: ['./cause-for-cancellation.component.css']
+})
+export class CauseForCancellationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
