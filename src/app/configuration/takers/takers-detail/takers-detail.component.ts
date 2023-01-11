dff710bf5f511999a1313f71d185143a3ce78b8d
Jhon Alen <jalen@compuamerica.com.ve>
2023-01-11 14:08:02 -0400

    se cambio el campo xtipo por ctarifa_exceso para usar el store procedures

---
+++ src/app/configuration/takers/takers-detail/takers-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-takers-detail',
+  templateUrl: './takers-detail.component.html',
+  styleUrls: ['./takers-detail.component.css']
+})
+export class TakersDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/configuration/takers/takers-index/takers-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-takers-index',
+  templateUrl: './takers-index.component.html',
+  styleUrls: ['./takers-index.component.css']
+})
+export class TakersIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
